1fa5f582095c11e3b65ca06e92e235c42e2c5c3a
Abdelmonem Adel <abdelmonemadel9@gmail.com>
2026-04-16 10:43:53 +0200

    Update Security in Sorce Pages

--- Backend/src/routes/uniqueInventoryRoutes.js
+++ Backend/src/routes/uniqueInventoryRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import UniqueInventory from "../DB/models/uniqueInventory.model.js";
 
+import { authMiddleware } from "../middlewares/auth.js";
+
 const router = express.Router();
 
 // GET all unique inventory summary
-router.get("/unique-inventory-summary", async (req, res) => {
+router.get("/unique-inventory-summary", authMiddleware, async (req, res) => {
   try {
     const data = await UniqueInventory.find({}).sort({ date: 1 });
     res.json(data);
